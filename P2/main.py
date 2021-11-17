962d6e78f4269fff79fae60fa7312cbd0147d671
John Mays <25927827+johnmays@users.noreply.github.com>
2021-11-16 19:29:51 -0500

    ✨ 1c half done.  Essentially yesterday's commit.

--- P2/main.py
+++ P2/main.py
@@ -2,10 +2,13 @@ import data_generator
 import numpy as np
 import matplotlib.pyplot as plt
 
+m = np.array([0.6,1.8])
+b = -5.73
+
 sepal_length, sepal_width, petal_length, petal_width, species = data_generator.iris_data_generator('data/irisdata.csv')
 
 
-def plot_data():
+def plot_iris_data():
     versicolor_petal_length = []
     versicolor_petal_width = []
     virginica_petal_length = []
@@ -32,5 +35,44 @@ def plot_data():
     plt.show()
 
 
-plot_data()
+def decision_boundary(x_one):
+    """Helper function to plot the decision boundary"""
+    x_two = (-m[0]/m[1])*x_one-(b/m[1])
+    return x_two
 
+
+def plot_iris_data_with_decision_boundary():
+    versicolor_petal_length = []
+    versicolor_petal_width = []
+    virginica_petal_length = []
+    virginica_petal_width = []
+    for l, w, s in zip(petal_length, petal_width, species):
+        if s == 'versicolor':
+            versicolor_petal_length.append(l)
+            versicolor_petal_width.append(w)
+        elif s == 'virginica':
+            virginica_petal_length.append(l)
+            virginica_petal_width.append(w)
+    # drawing the line
+    x_ones = np.linspace(0, 7.5, 75)
+    x_twos = []
+    for x_one in x_ones:
+        x_twos.append(decision_boundary(x_one))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.scatter(versicolor_petal_length, versicolor_petal_width, color='indigo', alpha=0.5, label='Versicolor')
+    ax.scatter(virginica_petal_length, virginica_petal_width, color='orchid', alpha=0.5, label='Virginica')
+    plt.plot(x_ones, x_twos, color='black')
+    plt.title("Iris Data")
+    plt.ylabel("Petal Width (cm)")
+    plt.xlabel("Petal Length (cm)")
+    plt.xlim(0, 7.5)
+    plt.ylim(0.8, 2.6)
+    plt.legend()
+    plt.show()
+
+
+plot_iris_data_with_decision_boundary()
